8bdc1c80797a8f83e96353ade700639bb297f202
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-06-28 18:15:13 +0300

    add the route for nav home and catagories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import Home from './components/Home';
+import Catagories from './components/categoriesPage';
+import NavBar from './components/NavBar';
+
 function App() {
-  return (<div />);
+  return (
+    <>
+
+      <NavBar />
+      <Routes>
+        <Route path="/" elemnent={<Home />} />
+        <Route path="/catagories" elemnent={<Catagories />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
