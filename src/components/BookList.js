170129b3e0814b9e2ba016c461655455d1c36e6d
beteland123 <91183694+beteland123@users.noreply.github.com>
2023-07-05 23:17:13 +0300

    update the list to render from the api

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,20 +1,27 @@
-import { useSelector } from 'react-redux';
-import Deletebtn from './Deletebtn';
+import { useSelector, useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchbook, getbook } from '../redux/books/booksSlice';
 
 function BookList() {
-  const { bookList } = useSelector((state) => state.books);
+  const books = useSelector(getbook);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchbook());
+  }, []);
+  if (!books || books.length === 0) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
-      {bookList.map((book) => (
+      {books.map((book) => (
         <ul key={book.item_id}>
           <li key={book.item_id}>{book.category || ''}</li>
           <li key={book.item_id}><span>{book.title}</span></li>
-          <li key={book.item_id}><span>{book.author}</span></li>
           <li key={book.item_id}>
             <ul>
               <li>comments</li>
               <li key={book.item_id}>
-                <Deletebtn id={book.item_id} />
                 {' '}
               </li>
               <li key={book.item_id}>Edit</li>
